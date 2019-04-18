ef185f35c13dd49fae20c9ec19b967815d15ee2b
StonedHesus <33186972+StonedHesus@users.noreply.github.com>
2019-04-18 19:00:28 +0300

    Update lora2.py

--- test/lora2.py
+++ test/lora2.py
@@ -6,5 +6,5 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 while True:
-    s.send('Ping')
+    s.send('Marco')
     time.sleep(5)
